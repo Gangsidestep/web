5ee8d45fdd3870b8b99d23b206352b5d39a8e261
david <david@typingmetrics.com>
2026-05-31 17:16:15 +0200

    Ensure fixed background is behind content (z-index:-1) to prevent overlaying localized pages

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ $canonicalUrl = "https://mydropintheoceans.org/";
 <body style="border: 0;margin: 0;padding: 0;background-color: #080a23;">
   <div style="max-width: 1000px !important;margin-right: auto;margin-left: auto;">
 		<?php include __DIR__ . '/php/site-header.php'; ?>
-	<div style="background-size: cover; width:100%;height:120%;background-image: url(/images/background/MyDIO_back.jpg);background-repeat: no-repeat;background-position-x: center;background-position-y: 74px;position:fixed;max-width:1000px;">
+	<div style="background-size: cover; width:100%;height:120%;background-image: url(/images/background/MyDIO_back.jpg);background-repeat: no-repeat;background-position-x: center;background-position-y: 74px;position:fixed;max-width:1000px;z-index:-1;">
 	</div>
 	
 	
